d43f418f32020799ce8ea6ca4e1560e7b475c791
Scott Lozano <scottloz@protonmail.com>
2023-05-08 13:56:43 +0100

    adds layout boilerplate

--- app.rb
+++ app.rb
@@ -3,9 +3,16 @@ require "sinatra/reloader" if development?
 require "pry-byebug"
 
 get "/" do
+  @usernames = ["ssaunier", "Papillard"]
   erb :index
 end
 
 get "/about" do
+  @usernames = ["ssaunier", "Papillard"]
   erb :about
+end
+
+get "/team/:username" do
+  puts params[:username]
+  "The username is #{params[:username]}"
 end
